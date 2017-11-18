5a56afce2791750f2220b50179e533e602a74522
tan chang sheng <changsheng@tans-MacBook-Pro.local>
2017-11-18 15:21:58 +0800

    Rename Vectoriser.java to  TfidfVectorizer.java

--- project/src/aa/vectorisation/TfidfVectorizer.java
+++ project/src/aa/vectorisation/TfidfVectorizer.java
@@ -3,7 +3,7 @@ package aa.vectorisation;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Vectoriser {
+public class TfidfVectorizer {
 
     private static Map<String, Integer> vocab = new HashMap<>();
 
